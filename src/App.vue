2e30906d46f24da721e51444e5d5db728337e912
“xrk-hhh” <xxjh2487657826@outlook.com>
2026-08-15 00:15:09 +0800

    fix: 回顶按钮改右上角悬浮（v1.2.x）

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import ParticleBackground from '@/components/particles/ParticleBackground.vue'
 import AppNav from '@/components/layout/AppNav.vue'
 import AppFooter from '@/components/layout/AppFooter.vue'
 import CommandPalette from '@/components/overlay/CommandPalette.vue'
+import BackToTop from '@/components/layout/BackToTop.vue'
 import { useKonami } from '@/composables/useKonami'
 
 const konami = ref(false)
@@ -27,6 +28,7 @@ useKonami(() => {
     </main>
     <AppFooter />
     <CommandPalette />
+    <BackToTop />
     <Transition name="fade">
       <div v-if="konami" class="pointer-events-none fixed bottom-8 left-1/2 z-[70] -translate-x-1/2">
         <p class="glow-text rounded-full border border-primary/40 bg-bg/90 px-5 py-2 text-sm">
